92eab7b29e58f31d6cb13f03e646de9965f1d328
myfreeformarchive-lab <myfreeformarchive@gmail.com>
2026-02-04 00:26:06 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // ✅ Scan HTML and Javascript for Tailwind classes
   content: ["./index.html", "./app.js"],
-  safelist: ["w-12", "h-12"],
+  safelist: ["w-12", "h-12", "w-16", "h-16", "w-20", "h-20", "w-24", "h-24", "w-32", "h-32"],
   theme: {
     extend: {
       fontFamily: { 
