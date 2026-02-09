a81d1dcbc90e0d350e94967ab893b8dd13fcef38
myfreeformarchive-lab <myfreeformarchive@gmail.com>
2026-02-09 07:41:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+window.db = db;
 
 // ==========================================
 // 1. STATE & DOM
