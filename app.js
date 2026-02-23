061295376dac68f603e700a4d16c5e89c9858e98
myfreeformarchive-lab <myfreeformarchive@gmail.com>
2026-02-23 10:59:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1593,12 +1593,12 @@ window.renderMessages = function(roomId) {
                 <div class="max-w-[80%] rounded-2xl px-4 py-2 ${
                     isMe 
                     ? 'bg-brand-500 text-white rounded-tr-none' 
-                    : 'bg-slate-100 text-slate-800 rounded-tl-none'
+                    : 'bg-slate-100 lg:bg-slate-200 text-slate-800 rounded-tl-none'
                 }">
                     <p class="text-sm">${msg.text}</p>
-                    <p class="text-[10px] opacity-70 mt-1 ${isMe ? 'text-right' : 'text-left'}">
-                        ${new Date(msg.timestamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
-                    </p>
+                    <p class="text-[10px] opacity-70 mt-1 flex ${isMe ? 'justify-end' : 'justify-start'}">
+    ${new Date(msg.timestamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+</p>
                 </div>
             </div>
         `;
