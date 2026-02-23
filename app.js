6666014d0765bbe1be5c40e5a6a84502156f6f1a
myfreeformarchive-lab <myfreeformarchive@gmail.com>
2026-02-23 12:21:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3309,7 +3309,9 @@ document.addEventListener('visibilitychange', () => {
 });
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js');
+  navigator.serviceWorker.register('sw.js')
+    .then(reg => console.log('SW Registered!', reg))
+    .catch(err => console.error('SW Registration Failed', err));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
