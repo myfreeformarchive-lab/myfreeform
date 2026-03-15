98fc07e77850e4309f609292fdd4fc72ff52feec
myfreeformarchive-lab <myfreeformarchive@gmail.com>
2026-03-15 20:46:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2214,7 +2214,7 @@ function openInputModal() {
 
   DOM.inputModal.classList.remove('hidden');
   document.body.style.overflow = 'hidden';
-  setTimeout(() => DOM.input.focus(), 500);
+  setTimeout(() => DOM.input.focus(), 400);
 }
 
 // Closes the compose sheet and restores scroll.
